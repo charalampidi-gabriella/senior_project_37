69d4ed9d377ff369646406a9f04107a63d843322
jkolby <70547924+jkolby@users.noreply.github.com>
2024-04-30 13:43:19 -0700

    updated scripts and added entry to run scripts at boot
    bash.desktop file is located under /home/jetauto/.config/autostart

--- object_recognition.sh
+++ object_recognition.sh
@@ -3,9 +3,9 @@ title2="tab 2"
 title3="tab 3"
 
 cmd1="sudo systemctl stop start_app_node.service; roscore"
-cmd2="sleep 5; roslaunch jetauto_peripherals usb_cam.launch"
-#cmd3="sleep 10; rosrun image_view image_view image:=/usb_cam/image_raw"
-cmd3="sleep 10; cd /home/jetauto/catkin_ws/src/darknet_ros/darknet_ros; roslaunch darknet_ros darknet_ros.launch"
+cmd2="sleep 27; roslaunch jetauto_peripherals usb_cam.launch"
+#cmd3="sleep 12; rosrun image_view image_view image:=/usb_cam/image_raw"
+cmd3="sleep 32; cd /home/jetauto/catkin_ws/src/darknet_ros/darknet_ros; roslaunch darknet_ros darknet_ros.launch"
 
 gnome-terminal --tab --title="$title1" --command="bash -c '$cmd1; $SHELL'" \
                --tab --title="$title2" --command="bash -c '$cmd2; $SHELL'" \
